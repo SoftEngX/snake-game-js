bc22637c1a1e5ad5cca87ee5b457abdb41c27550
Bekbolsun Akimov <akimovbekbolsun332@gmail.com>
2026-01-30 16:06:34 +0600

    Add snake board

---
+++ snake.js
@@ -0,0 +1,23 @@
+
+
+// board
+
+var blockSize = 25;
+var rows = 20;
+var cols = 20;
+var board;
+var context;
+
+window.onload = function() {
+    board = document.getElementById("board");
+    board.height = rows * blockSize;
+    board.width = cols * blockSize;
+    context = board.getContext("2d") // used for driwing on the board
+
+    update();
+}
+
+function update() {
+    context.fillStyle = "black";
+    context.fillRect(0, 0, board.width, board.height);
+}
